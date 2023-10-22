200ed18eb6e792b9996a4c60601165631f04eeeb
ogo001 <96411555+ogo001@users.noreply.github.com>
2023-10-22 20:25:57 +0200

    Added Initial rate
    Added methods for picking initial rate, and calculatiing the regression of the initial rate

--- src/data_processing.py
+++ src/data_processing.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 from data_calculations import calibration_regression, signal_to_concentration,\
-    baseline_regression, baseline_correction_function, calibration_mean
+    baseline_regression, baseline_correction_function, calibration_mean, initial_rate_regression
 
 
 class RawPlot:
@@ -72,6 +72,7 @@ class BaseLineCorrection:
     def plot_regression_baseline(self):
 
         data = self.baseline_data()
+        print(data)
 
         bsl_coeff, _, ypred = baseline_regression(data,self.df)
 
@@ -235,6 +236,93 @@ class PlotDataStart:
 
         df = pd.DataFrame({'x':x[start:],'y':y[start:]})
         return df
+
+class InitialRateDetermination:
+
+    def __init__(self):
+
+        self.df = None
+        self.int_rate = {}
+        self.x = []
+        self.y = []
+
+    def dict_to_dataframe(self,dictionary):
+
+        df = pd.DataFrame(columns=['Filename', 'Time (s)', '[H2O2]'])
+
+        # Iterate through the dictionary and append 'a' and 'y' values as rows
+        for filename, values in dictionary.items():
+            if 'Time (s)' in values and '[H2O2]' in values:
+                x_values = values['Time (s)']
+                y_values = values['[H2O2]']
+                temp_df = pd.DataFrame(
+                    {'Filename': [filename] * len(x_values), 'Time (s)': x_values, '[H2O2]': y_values})
+                df = pd.concat([df, temp_df], ignore_index=True)
+
+
+        self.df = df
+
+    @property
+    def get_colnames(self) -> list:
+        return [col for col in self.df.columns]
+
+    def initial_rate_picker(self, event) -> None:
+        column_names = self.get_colnames
+        ind = event.ind
+
+        x = np.take(self.df[column_names[1]], ind)
+        y = np.take(self.df[column_names[2]], ind)
+
+        self.x.extend(x)
+        self.y.extend(y)
+
+
+    def pick_initial_rate_points(self):
+        col_names = self.get_colnames
+
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111)
+        col = ax1.scatter(self.df[col_names[1]], self.df[col_names[2]], picker=True)
+
+        fig.canvas.mpl_connect('pick_event', self.initial_rate_picker)
+        plt.title('Select initial rate')
+
+        plt.show()
+
+
+    def plot_picked_points(self):
+
+
+        col_names = self.get_colnames
+        plt.scatter(self.df[col_names[1]], self.df[col_names[2]], s=3)
+
+        plt.scatter(self.x,self.y, s=5, color='red')
+        plt.title('Picked calibration points')
+        plt.show()
+
+
+
+    def initial_rate_data(self) -> list:
+        self.int_rate['Values'] = {'x':self.x,'y':self.y}
+        print(self.int_rate)
+        return self.int_rate
+
+    def plot_regression_initial_rate(self):
+
+        data = self.initial_rate_data()
+
+
+        bsl_coeff, _, ypred = initial_rate_regression(self.x,self.y)
+
+        x = np.array(data['Values']['x']).reshape(-1, 1)
+        y = np.array(data['Values']['y']).reshape(-1, 1)
+
+        plt.scatter(self.df['Time (s)'], self.df['[H2O2]'])
+        plt.scatter(x, y, color='red')
+        plt.plot(x, ypred)
+        plt.title('Selected baseline regression')
+        plt.show()
+
 
 
 
