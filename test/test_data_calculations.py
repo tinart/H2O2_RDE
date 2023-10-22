0f4cdd3763862c4ce2f4ee505d0a2970e5ab8c67
ogo001 <96411555+ogo001@users.noreply.github.com>
2023-10-22 16:51:49 +0200

    Tests for data_calculations.py
    Added tests for the data_calculations file

--- test/test_data_calculations.py
+++ test/test_data_calculations.py
@@ -1,14 +1,39 @@
-import pandas as pd
-import pytest
 
 
 import src.data_calculations
 
-@pytest.fixture()
-def baseline_test_data():
-    data = {}
-    data['Values'] = {'x':[0,1,2,3,4,5,6,7,8,9,10],'y':[0,2,4,6,8,10,12,14,16,18,20]}
-    return data
+
+import pytest
+import numpy as np
+import pandas as pd
+from data_calculations import fit_linear_regression, baseline_regression, baseline_correction_function, calibration_mean
+
+
+def test_fit_linear_regression():
+    x = [1, 2, 3, 4, 5]
+    y = [2, 4, 3, 4, 5]
+    coef, intercept, ypred = fit_linear_regression(x, y)
+    assert isinstance(coef, np.ndarray)
+    assert isinstance(intercept, np.ndarray)
+    assert isinstance(ypred, np.ndarray)
+
 
 def test_baseline_regression():
-    pass
+    data = {'Values': {'x': [1, 2, 3, 4, 5], 'y': [2, 4, 3, 4, 5]}}
+    coef, intercept, ypred = baseline_regression(data, None)
+    assert isinstance(coef, np.ndarray)
+    assert isinstance(intercept, np.ndarray)
+    assert isinstance(ypred, np.ndarray)
+
+
+def test_baseline_correction_function():
+    data = pd.DataFrame({'Time (s)': [1, 2, 3], 'WE(1).Current (A)': [0.1, 0.2, 0.3]})
+    bsl_coefficient = np.array([0.01])
+    corrected_df = baseline_correction_function(data, bsl_coefficient)
+    assert isinstance(corrected_df, pd.DataFrame)
+
+
+def test_calibration_mean():
+    data = pd.DataFrame({'Concentration': ['1', '1', '2', '2'], 'Current': [0.1, 0.2, 0.3, 0.4]})
+    mean_df = calibration_mean(data)
+    assert isinstance(mean_df, pd.DataFrame)
