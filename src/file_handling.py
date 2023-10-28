8a4f3a2fa255b0e8b2ce8af9409ae915cf38f83c
ogo001 <96411555+ogo001@users.noreply.github.com>
2023-10-28 17:18:21 +0200

    Cleanup main
    Unused methods removed and methods that should be in filehandlin were transfered

--- src/file_handling.py
+++ src/file_handling.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import shutil
+from datetime import datetime
 
 
 
@@ -37,7 +38,7 @@ class ReadData:
 
     def get_dataframe(self, file) -> pd.DataFrame:
         data = self.data_cache[file]
-        col1, col2 = self.get_header(file)
+        col1, col2, col3, col4 = self.get_header(file)
         return pd.DataFrame({col1: data[col1], col2: data[col2]})
 
     def drop_nan_dataframe(self, file):
@@ -51,3 +52,22 @@ class ReadData:
     def move_file_after_analysis(self, file):
         file_path = os.path.join(self.path, file)
         shutil.move(file_path, os.path.join(self.path, 'Analyzed'))
+
+    def create_longform_dataframe(self,data_dictionary):
+        # Create an empty DataFrame
+        df = pd.DataFrame(columns=['Filename', 'Time (s)', '[H2O2]'])
+        formated_date = datetime.now().strftime('%y%m%d')
+
+        # Iterate through the dictionary and append 'a' and 'y' values as rows
+        for filename, values in data_dictionary.items():
+            if 'Time (s)' in values and '[H2O2]' in values:
+                x_values = values['Time (s)']
+                y_values = values['[H2O2]']
+                temp_df = pd.DataFrame(
+                    {'Filename': [filename] * len(x_values), 'Time (s)': x_values, '[H2O2]': y_values})
+                df = pd.concat([df, temp_df], ignore_index=True)
+
+        os.chdir(self.path)
+        df.to_csv(f'{formated_date}_longform_analyzed_data.csv')
+
+        return df
